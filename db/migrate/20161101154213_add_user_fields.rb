2a471893b7c899337d8bbc2152ea77c637633d6d
pthomas <pthomas@Retinal-Nexus.local>
2016-11-01 10:50:29 -0500

    add user migration

---
+++ db/migrate/20161101154213_add_user_fields.rb
@@ -0,0 +1,11 @@
+# ================================================
+# RUBY->MIGRATION->ADDUSERFIELDS =================
+# ================================================
+class AddUserFields < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+    end
+  end
+end
