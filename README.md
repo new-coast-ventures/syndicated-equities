0734178c6377eded4d4d4194e2484c40867ca324
pthomas <pthomas@Retinal-Nexus.local>
2016-11-03 14:27:25 -0500

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # syndicated-equities
 Syndicated Equities project
+
+TODO: Redactor or WYSIWYG text editor for notes admin
+TODO: Editing user profile
+TODO: Formatting Devise views
+TODO: Properly setup nav
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 # ================================================
 # RUBY->MODEL->USER ==============================
 # ================================================
-# TODO: validators
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -11,6 +10,7 @@ class User < ActiveRecord::Base
   has_many :addresses
   has_many :investments
   has_many :deals, through: :investments
+  accepts_nested_attributes_for :addresses
 
   validates_presence_of :first_name
   validates_presence_of :last_name
