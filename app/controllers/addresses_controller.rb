4d8f831014ecc79410abc0a3c4f4ef783437b1a4
pthomas <pthomas@Retinal-Nexus.local>
2016-11-03 09:51:58 -0500

    finish address and data controllers

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,27 +5,11 @@ class AddressesController < ApplicationController
   before_action :authenticate_user!
 
   # ----------------------------------------------
-  # NEW ------------------------------------------
-  # ----------------------------------------------
-
-  def new
-    @address = Address.new
-  end
-
-  # ----------------------------------------------
-  # EDIT -----------------------------------------
-  # ----------------------------------------------
-
-  def edit
-    @address = Address.find(params[:id])
-  end
-
-  # ----------------------------------------------
   # CREATE ---------------------------------------
   # ----------------------------------------------
 
   def create
-    @address = Address.new(params[:address])
+    @address = Address.new(address_params)
     if @address.save
       redirect_to :back, notice: 'Address saved'
     else
@@ -39,10 +23,34 @@ class AddressesController < ApplicationController
 
   def update
     @address = Address.find(params[:id])
-    if @address.update_attributes(params[:address])
+    if @address.update_attributes(address_params)
       redirect_to :back, notice: 'Address saved'
     else
       redirect_to :back, alert: "There was an issue saving this address. #{@address.errors.full_messages.to_sentence}"
     end
   end
+
+  # ----------------------------------------------
+  # DELETE ---------------------------------------
+  # ----------------------------------------------
+
+  def destroy
+    @address = Address.find(params[:id])
+    if @address.destroy
+      redirect_to :back, notice: 'Address removed'
+    else
+      redirect_to :back, alert: "There was an issue removing this address. #{@address.errors.full_messages.to_sentence}"
+    end
+  end
+
+  # ----------------------------------------------
+  # PRIVATE ---------------------------------------
+  # ----------------------------------------------
+
+  private
+
+  def address_params
+    params.require(:address).permit(:user_id, :line1, :line2, :city, :state, :zip)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  resources :deals
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
