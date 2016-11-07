cb4e91ddc17a193577e5a5a14bd62390144c86ad
pthomas <pthomas@Retinal-Nexus.local>
2016-11-07 10:58:32 -0600

    setup edit registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,10 +9,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, [address_attributes: [:user, :line1, :line2, :city, :state, :zip]])
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, [address_attributes: [:id, :line1, :line2, :city, :state, :zip]])
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, address: [:user_id, :line1, :line2, :city, :state, :zip])
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, [address_attributes: [:id, :line1, :line2, :city, :state, :zip]])
   end
 end
