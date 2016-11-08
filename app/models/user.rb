cb2e3def1b174151355543505696cf6256cd5484
pthomas <pthomas@Retinal-Nexus.local>
2016-11-08 12:11:02 -0600

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name
   validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :password
 
   accepts_nested_attributes_for :address
 
