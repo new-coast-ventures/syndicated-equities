1e74a9c5997195a0fd1f6e904b63fd8f5086fb96
pthomas <pthomas@Retinal-Nexus.local>
2016-11-09 09:32:34 -0600

    update forms controller

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -5,74 +5,11 @@ class FormsController < ApplicationController
   before_action :authenticate_user! unless Rails.env.development?
 
   # ----------------------------------------------
-  # INDEX ----------------------------------------
-  # ----------------------------------------------
-
-  def index
-    @forms = Form.all
-  end
-
-  # ----------------------------------------------
   # SHOW -----------------------------------------
   # ----------------------------------------------
 
   def show
     @form = Form.find(params[:id])
-  end
-
-  # ----------------------------------------------
-  # NEW ------------------------------------------
-  # ----------------------------------------------
-
-  def new
-    @form = Form.new
-  end
-
-  # ----------------------------------------------
-  # EDIT -----------------------------------------
-  # ----------------------------------------------
-
-  def edit
-    @form = Form.find(params[:id])
-  end
-
-  # ----------------------------------------------
-  # CREATE ---------------------------------------
-  # ----------------------------------------------
-
-  def create
-    @form = Form.new(form_params)
-    if @form.save
-      redirect_to :back, notice: 'Form saved'
-    else
-      redirect_to :back, alert: "There was an issue saving this form. #{@form.errors.full_messages.to_sentence}"
-    end
-  end
-
-  # ----------------------------------------------
-  # UPDATE ---------------------------------------
-  # ----------------------------------------------
-
-  def update
-    @form = Form.find(params[:id])
-    if @form.update_attributes(form_params)
-      redirect_to :back, notice: 'Form saved'
-    else
-      redirect_to :back, alert: "There was an issue saving this form. #{@form.errors.full_messages.to_sentence}"
-    end
-  end
-
-  # ----------------------------------------------
-  # DESTROY --------------------------------------
-  # ----------------------------------------------
-
-  def destroy
-    @form = Form.find(params[:id])
-    if @form.destroy
-      redirect_to :back, notice: 'Form removed'
-    else
-      redirect_to :back, alert: "There was an issue removing this form. #{@form.errors.full_messages.to_sentence}"
-    end
   end
 
   # ----------------------------------------------
