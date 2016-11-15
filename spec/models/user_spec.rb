66200267af123f44ed2efdcaf37a8ca90b9a7742
pthomas <pthomas@Retinal-Nexus.local>
2016-11-15 15:06:50 -0600

    complete user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = create(:user)
+    @investment1 = create(:investment)
+    @investment2 = create(:investment)
+    @user.investments = [@investment1, @investment2]
+  end
+
   it { should have_many(:investments) }
   it { should have_one(:address) }
   it { should have_many(:deals) }
+
+  it 'has a total_invested method that returns the total invested' do
+    expect(@user.total_invested).to eq(@investment1.amount_invested + @investment2.amount_invested)
+  end
+
+  it 'has a investments_sorted_by_date method that returns the investments sorted in descending order by date' do
+    @investment2.invested_on = Date.today
+    @investment2.save!
+
+    if @investment1.invested_on > @investment2.invested_on
+      expect(@user.investments_sorted_by_date).to eq([@investment1, @investment2])
+    else
+      expect(@user.investments_sorted_by_date).to eq([@investment2, @investment1])
+    end
+  end
+
 end
