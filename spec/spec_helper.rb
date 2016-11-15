15b02726e5a07771fb34d2f892d2a0a29c934d7f
pthomas <pthomas@Retinal-Nexus.local>
2016-11-15 14:03:59 -0600

    setup simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.start
 require 'capybara/rspec'
 require 'paperclip/matchers'
 require 'factory_girl_rails'
