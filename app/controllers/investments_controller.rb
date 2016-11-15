160f3508b45775a87794d010dbf531c4b7a7d534
pthomas <pthomas@Retinal-Nexus.local>
2016-11-15 16:45:13 -0600

    setup investment auth

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -9,7 +9,13 @@ class InvestmentsController < ApplicationController
   # ----------------------------------------------
 
   def show
-    @investment = Investment.find(params[:id])
+    @investment = Investment.find_by(id: params[:id])
+    if @investment && @investment.investor == current_user || current_user.admin?
+      render 'show'
+    else
+      flash[:alert] = 'Not authorized'
+      redirect_to main_app.root_path
+    end
   end
 
   # ----------------------------------------------
