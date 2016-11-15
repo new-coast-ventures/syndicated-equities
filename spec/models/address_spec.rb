e9977dd123c63ea6d867d5a47b8ed6b11a0d1362
pthomas <pthomas@Retinal-Nexus.local>
2016-11-15 14:49:17 -0600

    complete address tests

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
+  before do
+    @address = create(:address)
+  end
+
   it { should belong_to :user }
+
+  it 'has a name method that prints out a formatted version of the address' do
+    expect(@address.name).to eq(@address.line1 + ' ' + @address.city + ', ' + @address.state + ' ' + @address.zip)
+  end
 end
