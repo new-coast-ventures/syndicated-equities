1f58ab482372119ee9f1d32af3fd5c06c574e447
pthomas <pthomas@Retinal-Nexus.local>
2017-02-14 10:47:07 -0600

    add attachment display for notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214160504) do
+ActiveRecord::Schema.define(version: 20170214164250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,11 +62,15 @@ ActiveRecord::Schema.define(version: 20170214160504) do
 
   create_table "notes", force: :cascade do |t|
     t.integer  "deal_id"
-    t.string   "title",          null: false
-    t.text     "content",        null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "title",                 null: false
+    t.text     "content",               null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "investor_email"
+    t.string   "document_file_name"
+    t.string   "document_content_type"
+    t.integer  "document_file_size"
+    t.datetime "document_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
