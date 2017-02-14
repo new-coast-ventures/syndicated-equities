6eac6c87ca7a48ee3883fc2984ee4ed2eb365c0e
pthomas <pthomas@Retinal-Nexus.local>
2017-02-14 10:43:56 -0600

    setup global notes display

--- app/models/note.rb
+++ app/models/note.rb
@@ -7,4 +7,8 @@ class Note < ActiveRecord::Base
   validates_presence_of :deal
   validates_presence_of :title
   validates_presence_of :content
+
+  def self.global
+    where(global: true).order(created_at: :desc)
+  end
 end
