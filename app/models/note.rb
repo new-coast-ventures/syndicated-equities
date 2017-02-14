dd821252fefb8b6d4178bbad5ebe4ff97a09db2d
pthomas <pthomas@Retinal-Nexus.local>
2017-02-14 10:51:24 -0600

    add attachment to notes model

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,10 +3,12 @@
 # ================================================
 class Note < ActiveRecord::Base
   belongs_to :deal
+  has_attached_file :document
 
   validates_presence_of :deal
   validates_presence_of :title
   validates_presence_of :content
+  validates_attachment_content_type :document, content_type: ['application/pdf','application/msword','	application/vnd.openxmlformats-officedocument.wordprocessingml.document']
 
   def self.global
     where(global: true).order(created_at: :desc)
