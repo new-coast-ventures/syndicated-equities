b549e261ca8098a2f83f30bc5a91d2222bcb3486
Scott Kacyn <scott.kacyn@gmail.com>
2018-06-12 16:12:39 -0500

    Set default mailer address in devise config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'mg.syndicatedequities.com',
+    :domain         => 'investors.syndicatedequities.com',
     :authentication => :plain,
   }
 
