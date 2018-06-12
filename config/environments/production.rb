092ad84332521ede2d3e289427a963576fb68e37
Scott Kacyn <scott.kacyn@gmail.com>
2018-06-12 15:58:52 -0500

    Add config for MailGun email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Devise setup TODO: set correct host
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'mg.syndicatedequities.com' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'mg.syndicatedequities.com',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
 
 end
