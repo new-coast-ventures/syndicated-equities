5f0928eb7dd3cf593d8d3a6ed0de7e30187f49f2
Rob Schwartz <rob.schwartz@vibes.com>
2019-12-27 10:26:51 -0500

    remove migrations

--- db/migrate/20191220163034_add_property_to_notes.rb
+++
@@ -1,5 +0,0 @@
-class AddPropertyToNotes < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :notes, :property, foreign_key: true
-  end
-end
--- db/migrate/20191220163152_remove_notes_from_properties.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNotesFromProperties < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :properties, :note, foreign_key: true
-  end
-end
