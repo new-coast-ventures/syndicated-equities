cf3c69a46dc2759b706ed4600f50bb31f6e7f84b
Rob Schwartz <rob.schwartz@vibes.com>
2020-01-17 10:12:45 -0500

    fixed mailer domain

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -53,14 +53,14 @@ Rails.application.configure do
   #   }
   # }
 
-  config.action_mailer.default_url_options = { host: 'investors.syndicatedequities.com' }
+  config.action_mailer.default_url_options = { host: 'syndicatedequities-stage.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'investors.syndicatedequities.com',
+    :domain         => 'syndicatedequities-stage.herokuapp.com',
     :authentication => :plain,
   }
 
