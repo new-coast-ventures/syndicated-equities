81ff4e4de625fe1c1c8897599bda3f1daf403733
Rob Schwartz <rob.schwartz@vibes.com>
2020-01-17 10:25:53 -0500

    fixed mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host: ENV['BASE_URL'],
+    :domain         => ENV['BASE_URL'],
     :authentication => :plain,
   }
 
