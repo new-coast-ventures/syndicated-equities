9e9774667f0d336e75cdf5b8d1ded57f850d8d4a
Rob Schwartz <rob.schwartz@vibes.com>
2020-02-19 16:48:29 -0500

    added more logs

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -69,7 +69,7 @@ class InvestmentsController < ApplicationController
     end
   rescue => e  
     puts "%%%%%%%%%%%%%%%%%%#{e}%%%%%%%%%%%%%%%%%%%%%%%%"
-    puts "%%%%%%%%%%%%%%%%%%#{e.trace}%%%%%%%%%%%%%%%%%%%%%%%%"
+    puts "%%%%%%%%%%%%%%%%%%#{e.backtrace.join "\n"}%%%%%%%%%%%%%%%%%%%%%%%%"
     flash[:alert] = 'Sorry, that investment is not available.'
     redirect_to main_app.root_path
   end
