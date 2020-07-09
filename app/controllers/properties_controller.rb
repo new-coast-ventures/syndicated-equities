b5362a7731c7a86ffe3c5bd411323e6971d353c8
Rob Schwartz <rob.schwartz@vibes.com>
2020-07-09 07:35:56 -0400

    fixed bugs with new Open property

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -66,7 +66,6 @@ class PropertiesController < ApplicationController
     
     # create property
     property = Property.new(property_params)
-    property.status = "active"
 
     if property.save
 
