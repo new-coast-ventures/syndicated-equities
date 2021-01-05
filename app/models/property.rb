e437f1d1832aa28b9e30ccfbced842b29f3b3c30
Rob Schwartz <rob.schwartz@vibes.com>
2021-01-05 06:57:17 -0500

    updated CSV export to include last name and address details

--- app/models/property.rb
+++ app/models/property.rb
@@ -105,12 +105,12 @@ class Property < ActiveRecord::Base
   end
 
   def export_investors
-    headers = %w{name email address investor_entity}
+    headers = %w{FirstName LastName Email City State Zip InvestorEntity}
     csv = CSV.generate(headers: true) do |csv|
       csv << headers
       investments.each do |inv|
         user = inv.investor
-        csv << [user.name, user.email, user&.full_address, user&.csv_investor_entity]
+        csv << [user&.first_name, user&.last_name user.email, user&.city, user&.state, user&.zip_code, user&.csv_investor_entity]
       end
     end
 
